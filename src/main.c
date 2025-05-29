fcac3b8b3c21e79125e1fbb74f64607f946f88f1
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-05-28 22:50:22 -0400

    It can now extract ROM data now!

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "directoryReader.h"
+#include "romFile.h"
+#include "romIO.h"
 #include "romSet.h"
 #include "romSetPrinter.h"
 
 int main(int argc, char* argv[]) {
 	RomSet romSet = readDirectory(argv[1]);
-    printROMSet(&romSet);
+	RomFile* rom = getROM(&romSet, 3); // Let's see if we can get the first gfx ROM in Pac-Man.
+	readROM(rom);
+	printROMSet(&romSet);
+	printROMData(rom); // Print it all out!
 	return 0;
 }
